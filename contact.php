b8dad2cfb913477aa488cb4a4ab5e7dd312ec86a
Ayush Tiwari <47293577+dbcelm@users.noreply.github.com>
2020-06-07 11:20:46 +0530

    updated to include country and phone

--- contact.php
+++ contact.php
@@ -4,9 +4,11 @@
 		$email=$_POST['email'];
 		$subject=$_POST['subject'];
 		$msg=$_POST['msg'];
+		$phone=$_POST['phone'];
+		$country=$_POST['country'];	
 
 		$to='dbcelm@gmail.com'; // Receiver Email ID, Replace with your email ID
-		$message="Name :".$name."\n"."Email :".$email."\n"."Wrote the following :"."\n\n".$msg;
+		$message="Name :".$name."\n"."Email :".$email."\n"."Phone :".$phone."\n"."Country :".$country."\n"."Wrote the following :"."\n\n".$msg;
 		$headers="Subject: ".$subject;
 		$headers="From: dbcelm@gmail.com";
 
